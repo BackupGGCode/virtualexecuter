038f4bda6cf0f19de215fd277373ce8ef3feffa7
aboesx@gmail.com <aboesx@gmail.com@5f6647e1-f445-0410-9f00-03a561128185>
2008-02-18 19:39:33 +0000

    Added a folder remotely

---
+++ Software/VXC/ParserTest/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using VXC.lexer;
+using VXC.parser;
+using VXC.node;
+using System.IO;
+
+namespace ParserTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            StreamReader sr = new StreamReader("program.vxc");
+            Lexer l = new Lexer(sr);
+            Parser p = new Parser(l);
+            Start start = p.Parse();
+        
+            ASTDisplay disp = new ASTDisplay();
+            start.Apply(disp);
+            ASTDisplayForm form = new ASTDisplayForm();
+            form.treeView1.Nodes.Add(disp.result);
+
+            Application.Run(form);
+        }
+    }
+}
